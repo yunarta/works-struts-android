ad4479bf14e735aa8ac389cf446a7af131c78ea9
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2017-05-15 09:54:35 +0800

    Update struts

--- src/main/kotlin/com/mobilesolutionworks/android/viper/VIPERView.kt
+++ src/main/kotlin/com/mobilesolutionworks/android/viper/VIPERView.kt
@@ -1,7 +1,14 @@
 package com.mobilesolutionworks.android.viper
 
+import android.view.View
+
 /**
  * Created by yunarta on 14/5/17.
  */
 
-public interface VIPERView
+public interface VIPERView {
+
+    val root: View
+
+    fun finish()
+}
